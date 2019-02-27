5af58d30931f540fe6b6512904ae662996682c03
Tanapol Ariyakunathon <lotspecter@live.com>
2019-02-28 03:37:10 +0700

    Update for kargos

--- optimus-manager-ar.sh
+++ optimus-manager-ar.sh
@@ -1,33 +1,20 @@
 #!/usr/bin/env bash
 # Thanks for idea and base script cyberalex4life <3 
-nvidia_switch="optimus-manager --no-confirm --switch nvidia"
-intel_switch="optimus-manager --no-confirm --switch intel"
-notify_switch="notify-send -h int:transient:2 -i \\\"dialog-information-symbolic\\\" \\\"Optimus Manager Indicator\\\" \\\"Switching graphics and restaring X server to finalize process! \\\" ; "
-notify_reboot="notify-send -h int:transient:2 -i \\\"dialog-information-symbolic\\\"  \\\"Optimus Manager Indicator\\\" \\\"System will reboot! \\\" ; "
+nvidia_switch='optimus-manager --no-confirm --switch nvidia'
+intel_switch='optimus-manager --no-confirm --switch intel'
+notify_switch='notify-send -h int:transient:2 -i dialog-information-symbolic "Switching graphics and restaring X server to finalize process!" ; '
+notify_reboot='notify-send -h int:transient:2 -i dialog-information-symbolic  "System will reboot!" ; '
 
-activate_intel="\"\
-	$notify_switch \
-	sleep 2; \
-	$intel_switch \
-	\""
+activate_intel="$notify_switch sleep 2; $intel_switch"
 
-activate_nvidia="\"\
-	$notify_switch \
-	sleep 2; \
-	$nvidia_switch \
-	\""
+activate_nvidia="$notify_switch sleep 2; $nvidia_switch"
 
-reboot_cmd="\"\
-	$notify_reboot \
-	sleep 1; \
-	systemctl --no-wall reboot \
-	\""
+reboot_cmd="$notify_reboot sleep 1; systemctl --no-wall reboot"
 
-nvidia_settings="\"nvidia-settings -p 'PRIME Profiles'\""
-nvidia_active_icon="nvidiaactivesymbolic"
+#nvidia_settings='nvidia-settings -p "PRIME Profiles" '
+nvidia_active_icon="nvidia-active-symbolic"
 nvidia_inactive_icon="primeindicatorintelsymbolic"
 reboot_icon="'system-restart'"
-
 
 QUERY=$(optimus-manager --print-mode)
 if [ "$QUERY" == 'Current mode : nvidia' ]; then
@@ -41,10 +28,10 @@ fi
 
 echo -e "$panel_string""iconName=$nvidia_state_icon"
 echo "---"
-echo "NVIDIA PRIME Profiles| iconName=$nvidia_state_icon bash=$nvidia_settings terminal=false"
+echo "NVIDIA PRIME Profiles| iconName=$nvidia_state_icon bash='$nvidia_settings' terminal=false"
 echo "---"
-echo "Switch to INTEL | iconName='primeindicatorintel' bash=$activate_intel terminal=false"
-echo "Switch to NVIDIA  | iconName='primeindicatornvidia' bash=$activate_nvidia  terminal=false"
+echo "Switch to INTEL | iconName='primeindicatorintel' bash='$activate_intel' terminal=false"
+echo "Switch to NVIDIA  | iconName='primeindicatornvidia' bash='$activate_nvidia'  terminal=false"
 
 echo "---"
 echo "System Reboot | iconName=$reboot_icon bash=$reboot_cmd terminal=false"
